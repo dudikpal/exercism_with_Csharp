d91b30d3e554c84e75a26ce27f2eced4f7a15b3e
dudikpal <59420449+dudikpal@users.noreply.github.com>
2021-09-09 15:04:27 +0200

    DifferenceOfSquares done

---
+++ difference-of-squares/DifferenceOfSquares.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max)
+    {
+        int sum = 0;
+        for (int i = 1; i <= max; i++)
+        {
+            sum += i;
+        }
+        return (int)Math.Pow(sum, 2);
+    }
+
+    public static int CalculateSumOfSquares(int max)
+    {
+        int sum = 0;
+        for (int i = 1; i <= max; i++)
+        {
+            sum += i * i;
+        }
+        return sum;
+    }
+
+    public static int CalculateDifferenceOfSquares(int max)
+    {
+        return CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+    }
+}
