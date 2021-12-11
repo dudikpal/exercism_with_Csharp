4db90ffc6fabbefd18693bb65a1f050b63b64048
Dudik Pál <dudikpal@gmail.com>
2021-12-12 00:47:38 +0100

    out of range exception on line 106

--- rail-fence-cipher/RailFenceCipher.cs
+++ rail-fence-cipher/RailFenceCipher.cs
@@ -45,13 +45,15 @@ public class RailFenceCipher
         int verticalMove = 0;
         bool increase = true;
         List<char>[] encodedArrays = new List<char>[rails];
+        
         for (int i = 0; i < remain; i++)
         {
             if (i < rails)
             {
-                if (i == 0 || i == rails - 1)
+                if (i == 0)
                 {
                     arraysLength[i] = baseLength;
+                    arraysLength[rails - 1] = baseLength;
                 }
                 else
                 {
@@ -78,11 +80,42 @@ public class RailFenceCipher
                 verticalMove--;
             }
         }
-        
-        
-        Console.WriteLine(arraysLength);
-        
-        return null;
+
+        int startPosition = 0;
+        for (int i = 0; i < arraysLength.Length; i++)
+        {
+            int length = arraysLength[i];
+            encodedArrays[i] = new List<char>(input.Substring(startPosition, length).ToCharArray());
+            startPosition += length;
+        }
+
+        verticalMove = 0;
+        increase = true;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (verticalMove == rails - 1)
+            {
+                increase = false;
+            }
+            else if (verticalMove == 0)
+            {
+                increase = true;
+            }
+
+            sb.Append(encodedArrays[verticalMove][0]);
+            encodedArrays[verticalMove].RemoveAt(0);
+            if (increase)
+            {
+                verticalMove++;
+            }
+            else
+            {
+                verticalMove--;
+            }
+        }
+
+        return sb.ToString();
     }
     
     private string encodeString(string input, bool increase, int verticalMove)
